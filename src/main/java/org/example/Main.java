8f46c6d16c1f7dce36a010fbf9a109f97b501f34
Ardash55 <ardash30042005g@gmail.com>
2025-09-28 15:45:56 +0300

    Добавлен метод создания новых задач

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Todo firstTodo = new Todo("Первая заметка", "Текст заметки", 1);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Меню:");
+        System.out.println("1. Записать новую задачу");
+        System.out.println("2. Показать задачи");
+        int action = sc.nextInt();
+        switch (action) {
+            case 1:
+                makeNewTodo();
+                break;
+        }
+    }
 
-        System.out.println(firstTodo.getName() + ": " + firstTodo.getText() + ". [" + firstTodo.getId() + "]");
+    public static void makeNewTodo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите название задачи");
+        String name = sc.nextLine();
+        System.out.println("Введите текст задачи");
+        String text = sc.nextLine();
+        Todo newTodo = new Todo(name, text, 0);
+        System.out.println(newTodo.getName() + ": " + newTodo.getText() + ". [" + newTodo.getId() + "]");
     }
 }

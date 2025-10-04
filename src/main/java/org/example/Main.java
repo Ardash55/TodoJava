83202596aba43e8db1e52ccd4a67e7772df9c3ee
Ardash55 <ardash30042005g@gmail.com>
2025-10-04 18:31:35 +0300

    Создан список задач. Создане метод их просмтора. Метод создлания задач изменен, чтобы они автоматически добавлялись в общий список

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,43 @@
 package org.example;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
+    static ArrayList<Todo> todos = new ArrayList<>();
+    static  Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Меню:");
-        System.out.println("1. Записать новую задачу");
-        System.out.println("2. Показать задачи");
-        int action = sc.nextInt();
-        switch (action) {
-            case 1:
-                makeNewTodo();
-                break;
+        while (true) {
+            System.out.println("Меню:");
+            System.out.println("1. Записать новую задачу");
+            System.out.println("2. Показать задачи");
+            int action = sc.nextInt();
+            switch (action) {
+                case 1:
+                    makeNewTodo();
+                    break;
+                case 2:
+                    showTodos();
+                    break;
+            }
         }
     }
 
     public static void makeNewTodo() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Введите название задачи");
         String name = sc.nextLine();
         System.out.println("Введите текст задачи");
         String text = sc.nextLine();
         Todo newTodo = new Todo(name, text, 0);
         System.out.println(newTodo.getName() + ": " + newTodo.getText() + ". [" + newTodo.getId() + "]");
+        todos.add(newTodo);
+        System.out.println("");
+    }
+
+    public static void showTodos() {
+        System.out.println("Ваши задачи:");
+        for (Todo todo : todos) {
+            System.out.println(todo.getName() + ": " + todo.getText() + ". [" + todo.getId() + "]");
+        }
+        System.out.println("");
     }
 }
